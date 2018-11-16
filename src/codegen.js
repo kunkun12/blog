0971940d21c764df746ecda5a9c4332a5989e8ab
dubaokun <dubaokun@xiaomi.com>
2018-11-16 19:51:02 +0800

    update

--- src/codegen.js
+++ src/codegen.js
@@ -3,3 +3,6 @@ const array = ['apple', 'orange', 'pear']
 module.exports = array
   .map(fruit => `export const ${fruit} = '${fruit}';`)
   .join('')
+
+
+  //codegen`module.exports=(function(){return (1+1).toString()}())`
